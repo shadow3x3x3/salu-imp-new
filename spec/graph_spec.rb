1c9e4d7872e21e93c7092f0c12172cfd96a96175
shadow3x3x3 <shadow3x3x3@gmail.com>
2016-07-28 18:46:46 +0800

    remove constant PATH

--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -1,11 +1,8 @@
 require_relative 'spec_helper'
 require_relative '../structure/graph'
 
-EDGE_PATH = './salu-data/salu_edge_160203_450mm_z.txt'
-NODE_PATH = './salu-data/salu_node_160203.txt'
-
-test_edges = File.read(EDGE_PATH)
-test_nodes = File.read(NODE_PATH)
+test_edges = File.read('./salu-data/salu_edge_160203_450mm_z.txt')
+test_nodes = File.read('./salu-data/salu_node_160203.txt')
 
 describe Graph do
   let(:g) { Graph.new(raw_edges: test_edges, raw_nodes: test_nodes) }
